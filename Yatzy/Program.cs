a9bb60bc1b073e4a9cb3ae739276ac3b9e456b6a
Axel F <axel.friman@elev.ga.ntig.se>
2022-09-14 09:41:11 +0200

    lesson2

--- Yatzy/Program.cs
+++ Yatzy/Program.cs
@@ -21,27 +21,55 @@ namespace Yatzy
         }
         public static int[] RollDices(int[] DiceValue)
         {
-            int rolls = 0;
             Random rnd = new Random();
 
             for (int i = 0; i < DiceValue.Length; i++)
             {
                 DiceValue[i] = rnd.Next(1, 6);
             }
-            rolls += 1;
 
             Console.WriteLine("Du fick: ");
             for (int i = 0; i < DiceValue.Length; i++)
             {
                 Console.WriteLine(DiceValue[i]);
             }
+
             Console.WriteLine("Vill du slå om någon tärning?");
             string Reroll = Console.ReadLine().ToLower();
             if(Reroll == "ja")
             {
-                Console.WriteLine("Vilken/vilka tärningar vill du slå om?");
+                RerollDices(DiceValue);
+                Console.WriteLine("Vill du slå om någon tärning igen?");
+                string RerollAgain = Console.ReadLine().ToLower();
+                if(RerollAgain == "ja")
+                {
+                    RerollDices(DiceValue);
+                }
             }
             return DiceValue;
+        }
+        public static int[] RerollDices(int[] DiceValue)
+        {
+            int[] RerollDices = new int[5];
+            int i = 0;
+            Random rnd = new Random();
+            string WantToReroll = "ja";
+            
+            while(WantToReroll == "ja" && i < 5)
+            {
+                Console.WriteLine("Vilken tärning vill du slå om?");
+                RerollDices[i] = (Int32.Parse(Console.ReadLine()) - 1);
+                DiceValue[RerollDices[i]] = rnd.Next(1, 6);
+                Console.WriteLine("Vill du slå om någon mer tärning?");
+                WantToReroll = Console.ReadLine().ToLower();
+            }
+            Console.WriteLine("Du fick: ");
+            for (int j = 0; j < DiceValue.Length; j++)
+            {
+                Console.WriteLine(DiceValue[j]);
+            }
+            return DiceValue;
+
         }
     }
 }
