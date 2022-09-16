617b631b9c0531f324f4230ad547922e1be09f05
Axel F <axel.friman@elev.ga.ntig.se>
2022-09-16 14:20:35 +0200

    Börjat på spar-funktionen av tärningar

--- Yatzy/Program.cs
+++ Yatzy/Program.cs
@@ -6,18 +6,38 @@ namespace Yatzy
     {
         static void Main(string[] args)
         {
+            int[] player1DiceValue = new int[5];
+            int[] player2DiceValue = new int[5];
+
             Console.WriteLine("Spelare 1 skriv in ditt användarnamn!");
             string player1 = Console.ReadLine();
             Console.WriteLine("Spelare 2 skriv in ditt användarnamn!");
             string player2 = Console.ReadLine();
-            PlayRound(player1);
+
+            int round = 0;
+            while(round < 6)
+            {
+                player1DiceValue = PlayRound(player1, player1DiceValue);
+                player2DiceValue = PlayRound(player2, player2DiceValue);
+                round += 1;
+            }
+            for (int i = 0; i < player1DiceValue.Length; i++)
+            {
+                Console.WriteLine($"Spelare 1 {player1DiceValue[i]}");
+                Console.WriteLine($"Spelare 2 {player2DiceValue[i]}");
+            }
+            
         }
-        public static void PlayRound(string name)
+        public static int[] PlayRound(string name, int[] playerDiceValue)
         {
             int[] DiceValue = new int[5];
+
             Console.WriteLine($"{name} det är din tur att slå tärningarna!");
+            Console.ReadLine();
             DiceValue = RollDices(DiceValue);
-            
+            playerDiceValue = SaveDiceValue(DiceValue, playerDiceValue);
+
+            return playerDiceValue;
         }
         public static int[] RollDices(int[] DiceValue)
         {
@@ -31,29 +51,28 @@ namespace Yatzy
             Console.WriteLine("Du fick: ");
             for (int i = 0; i < DiceValue.Length; i++)
             {
-                Console.WriteLine(DiceValue[i]);
+                Console.WriteLine($"Tärning {i + 1}: {DiceValue[i]}");
             }
 
             Console.WriteLine("Vill du slå om någon tärning?");
             string Reroll = Console.ReadLine().ToLower();
             if(Reroll == "ja")
             {
-                RerollDices(DiceValue);
+                RerollDices(DiceValue, Reroll);
                 Console.WriteLine("Vill du slå om någon tärning igen?");
                 string RerollAgain = Console.ReadLine().ToLower();
                 if(RerollAgain == "ja")
                 {
-                    RerollDices(DiceValue);
+                    RerollDices(DiceValue, RerollAgain);
                 }
             }
             return DiceValue;
         }
-        public static int[] RerollDices(int[] DiceValue)
+        public static int[] RerollDices(int[] DiceValue, string WantToReroll)
         {
             int[] RerollDices = new int[5];
             int i = 0;
             Random rnd = new Random();
-            string WantToReroll = "ja";
             
             while(WantToReroll == "ja" && i < 5)
             {
@@ -66,10 +85,28 @@ namespace Yatzy
             Console.WriteLine("Du fick: ");
             for (int j = 0; j < DiceValue.Length; j++)
             {
-                Console.WriteLine(DiceValue[j]);
+                Console.WriteLine($"Tärning {j + 1}: {DiceValue[j]}");
             }
             return DiceValue;
 
+        }
+        public static int[] SaveDiceValue(int[] DiceValue, int[] playerDiceValue)
+        {
+            Console.WriteLine("Vilken siffra vill du spara på?");
+            int ValueToSave = Int32.Parse(Console.ReadLine());
+            while(playerDiceValue[ValueToSave - 1] != 0)
+            {
+                Console.WriteLine($"Du har redan tagit {ValueToSave}, ta något annat");
+                ValueToSave = Int32.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < DiceValue.Length; i++)
+            {
+                if(DiceValue[i] == ValueToSave)
+                {
+                    playerDiceValue[ValueToSave - 1] += DiceValue[i];
+                }
+            }
+            return playerDiceValue;
         }
     }
 }
